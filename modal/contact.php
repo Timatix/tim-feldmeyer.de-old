dcddc1b68abdf35d6629a26b6c6026bd52cc5b69
Timatix <hi@timatix.eu>
2022-08-11 14:01:04 +0200

    fixed contact form

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "mail+contactform@tim-feldmeyer.de";
+$php_main_email = "mail@tim-feldmeyer.de";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
@@ -19,7 +19,7 @@ $php_email = filter_var($php_email, FILTER_SANITIZE_EMAIL);
 if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
 	
 	
-		$php_subject = "Message from contact form";
+		$php_subject = "Nachricht über das Kontaktformulat - Tim Feldmeyer";
 		
 		// To send HTML mail, the Content-type header must be set
 		$php_headers = 'MIME-Version: 1.0' . "\r\n";
@@ -27,15 +27,15 @@ if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
 		$php_headers .= 'From:' . $php_email. "\r\n"; // Sender's Email
 		$php_headers .= 'Cc:' . $php_email. "\r\n"; // Carbon copy to Sender
 		
-		$php_template = '<div style="padding:50px;">Hello ' . $php_name . ',<br/>'
-		. 'Thank you for contacting us.<br/><br/>'
-		. '<strong style="color:#f00a77;">Name:</strong>  ' . $php_name . '<br/>'
-		. '<strong style="color:#f00a77;">Email:</strong>  ' . $php_email . '<br/>'
-		. '<strong style="color:#f00a77;">Subject:</strong>  ' . $php_subject . '<br/>'
-		. '<strong style="color:#f00a77;">Message:</strong>  ' . $php_message . '<br/><br/>'
-		. 'This is a Contact Confirmation mail.'
+		$php_template = '<div style="padding:50px;">Hallo ' . $php_name . ',<br/>'
+		. 'Vielen Dank für deine Anfrage. Hier eine kurze Zusammenfassung deiner Anfrage:<br/><br/>'
+		. '<strong style="color:#2b2e83;">Name:</strong>  ' . $php_name . '<br/>'
+		. '<strong style="color:#2b2e83;">Email:</strong>  ' . $php_email . '<br/>'
+		. '<strong style="color:#2b2e83;">Betreff:</strong>  ' . $php_subject . '<br/>'
+		. '<strong style="color:#2b2e83;">Nachricht:</strong>  ' . $php_message . '<br/><br/>'
+		. 'Hiermit bestätige ich, dass die Nachricht bei mir eingegangen ist.'
 		. '<br/>'
-		. 'We will contact you as soon as possible .</div>';
+		. 'Ich werde mich umgehend melden.</div>';
 		$php_sendmessage = "<div style=\"background-color:#f5f5f5; color:#333;\">" . $php_template . "</div>";
 		
 		// message lines should not exceed 70 characters (PHP rule), so wrap it
